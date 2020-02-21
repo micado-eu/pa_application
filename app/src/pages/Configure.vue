c38e07f1ad2feaf96e7cb3201cbe90e7e1c2e6e8
Luca Gioppo <gioppoluca@libero.it>
2020-02-21 16:24:55 +0100

    feature

--- app/src/pages/Configure.vue
+++ app/src/pages/Configure.vue
@@ -1,12 +1,36 @@
 <template>
   <q-page class="flex flex-center">
-    <img alt="Micado logo" src="~assets/micado-logo.png">
-    Public Admiistration Console
+    <q-option-group
+      v-model="group"
+      :options="options"
+      color="yellow"
+      type="toggle"
+    />
+    {{group}}
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data () {
+    return {
+      group: [ 'FEATURE1' ],
+      options: [
+        {
+          label: 'Feature1',
+          value: 'FEATURE1'
+        },
+        {
+        label: 'Feature2',
+        value: 'FEATURE2'
+        },
+        {
+        label: 'Feature3',
+        value: 'FEATURE3'
+        }
+      ]
+    }
+  }
 }
 </script>
