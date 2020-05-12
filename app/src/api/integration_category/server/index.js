9485e4576cdfef9db6071830f18c7f4a9074daa5
Luca Gioppo <gioppoluca@libero.it>
2020-05-12 09:11:52 +0200

    added API call in integration category

--- app/src/api/integration_category/server/index.js
+++ app/src/api/integration_category/server/index.js
@@ -1,12 +1,40 @@
-import axios from 'axios'
+import { axiosInstance }  from 'boot/axios'
 
 export default {
   fetchIntegrationCategory () {
-    return axios
-      .get('https://jsonplaceholder.typicode.com/posts')
+    console.log("CALLING API!!!!")
+    return axiosInstance
+      .get('/db/v01/intervention_category?lang=eq.en')
       .then(response => response.data)
-  },
-  updateIntegrationCategory(integration_category){
-
+    },
+    updateIntegrationCategory(integration_category) {
+      console.log("fake call to update DB")
+      console.log(integration_category)
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(integration_category)
+        }, 0)
+      })
+    },
+    saveIntegrationCategory(integration_category) {
+      console.log("fake call to save to DB")
+      console.log(integration_category)
+      // create fake id here
+      integration_category.id = 999
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(integration_category)
+        }, 0)
+      })
+    },
+    deleteIntegrationCategory(integration_category) {
+      console.log("fake call to save to DB")
+      console.log(integration_category)
+      // create fake id here
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(integration_category)
+        }, 0)
+      })
+    }
   }
-}
