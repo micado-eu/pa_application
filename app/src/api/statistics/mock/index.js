1533c0faf2009af321b30a9993400e98160ac9a7
jowie <zyx1993@126.com>
2020-07-01 19:11:55 +0200

    clean up statistics/mock/index code

--- app/src/api/statistics/mock/index.js
+++ app/src/api/statistics/mock/index.js
@@ -1,37 +1,17 @@
 import timeseries from './data/timeseries'
 import origin from './data/origin'
 import migrants from './data/migrants'
-import city_graphs from './data/city/graphs.json'
-import categories from './data/city/categories.json'
-
-
-
-
-const fetch = (mockData, time = 0) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(mockData)
-    }, time)
-  })
-}
+import city_graphs from './data/city/graphs'
+import categories from './data/city/categories'
 
 export default {
   fetchStatistics() {
-    return Promise.all([
-      fetch(timeseries, 1000),
-      fetch(origin, 1000),
-      fetch(migrants, 1000),
-      fetch(city_graphs, 1000),
-      fetch(categories, 1000)])
-
-      .then((res) => {
-        return {
-          timeseries: res[0],
-          origin: res[1],
-          migrants: res[2],
-          city_graphs: res[3],
-          categories: res[4],
-        };
-      });
+    return Promise.resolve({
+      timeseries,
+      origin,
+      migrants,
+      city_graphs,
+      categories,
+    })
   }
 }
