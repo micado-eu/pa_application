4225983ade5962d6bba0b0fdd346ea1a9c2646da
Luca Gioppo <gioppoluca@libero.it>
2020-07-08 17:07:43 +0200

    added proper visualization of graph of processes

--- app/src/api/graphs/server/index.js
+++ app/src/api/graphs/server/index.js
@@ -1,10 +1,11 @@
-import axios from 'axios'
+import { axiosInstance } from 'boot/axios'
+import { error_handler } from '../../../helper/utility'
 
 export default {
-  fetchSteps () {
-    return axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.data)
-  }
+  fetchGraphs (id, userLang) {
+    return axiosInstance
+      .get('/backend/1.0.0/cytoscape?processid=' + id + '&lang=' + userLang)
+      .then(response => { return response.data })
+      .catch(error_handler);
+  },
 }
- 
