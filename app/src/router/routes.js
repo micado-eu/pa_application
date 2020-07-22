e8ed52fd9b87221430c8bdcdf7412cc5c0cc787e
giuseppecsi <giuseppe.dorio@csi.it>
2020-07-22 09:28:32 +0200

    fixed Adding new process leading to page not found

--- app/src/router/routes.js
+++ app/src/router/routes.js
@@ -40,7 +40,7 @@ const routes = [
       { path: '/adddocument/:id', component: AddDocument, props: true, name: document },
       { path: '/guided_process_editor', component: ProcessEditor, name: 'guidedprocesslist' },
 
-      //      { path: '/guided_process_editor/edit/:id', component: () => import('components/EditProcess.vue'), props: true, name: process },
+      { path: '/guided_process_editor/edit/', component: () => import('components/EditProcess.vue'), props: true, name: process },
       { path: '/guided_process_editor/edit/:theprocess', name: 'editprocess', component: () => import('components/EditProcess.vue'), props: (route) => ({ ...route.params }) },
       { path: '/guided_process_editor/edit/:processId/steps', component: () => import('pages/StepManager.vue'), props: (route) => ({ ...route.params }), name: 'editstep' },
 
