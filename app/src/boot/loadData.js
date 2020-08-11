f6e211a20cc0a497c598f30c2a96350637f23956
JowieXiang <zyx1993@126.com>
2020-08-11 12:21:25 +0200

    fix: error-handling for loadData

--- app/src/boot/loadData.js
+++ app/src/boot/loadData.js
@@ -1,22 +1,25 @@
 import { setLocale } from './i18n'
 
 export default async ({ store, Vue }) => {
-    await store.dispatch('statistics/fetchStatistics');
-    await store.dispatch('features/fetchFeatures');
-    await store.dispatch("language/fetchLanguages")
-    await store.dispatch('settings/fetchSettings')
-        .then(settings => {
-            console.log("#####################################")
-            console.log(settings)
-            var curlang = settings.filter((setting) => { return setting.key == 'default_language' })[0]
-            setLocale(curlang.value)
-            var defaultLangString = store.state.language.languages.filter(function (l) { return l.lang == curlang.value })[0].name
-            Vue.prototype.$defaultLangString = defaultLangString
-            Vue.prototype.$defaultLang = curlang.value
-            Vue.prototype.$userLang = curlang.value
-
-        })
-
+    try {
+        await store.dispatch('statistics/fetchStatistics');
+        await store.dispatch('features/fetchFeatures');
+        await store.dispatch("language/fetchLanguages")
+        await store.dispatch('settings/fetchSettings')
+            .then(settings => {
+                console.log("#####################################")
+                console.log(settings)
+                var curlang = settings.filter((setting) => { return setting.key == 'default_language' })[0]
+                setLocale(curlang.value)
+                var defaultLangString = store.state.language.languages.filter(function (l) { return l.lang == curlang.value })[0].name
+                Vue.prototype.$defaultLangString = defaultLangString
+                Vue.prototype.$defaultLang = curlang.value
+                Vue.prototype.$userLang = curlang.value
+            })
+    }
+    catch (err) {
+        console.log('Error: Quasar failed to load data from db')
+    }
 
 }
 
