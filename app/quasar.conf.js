e268b71022b159c752df317a59f259338959f662
Luca Gioppo <gioppoluca@libero.it>
2020-10-23 09:54:26 +0200

    added Countly

--- app/quasar.conf.js
+++ app/quasar.conf.js
@@ -11,7 +11,8 @@ module.exports = function (ctx) {
       'auth',
       'mermaid',
       'axios',
-      'loadData'
+      'loadData',
+      'countly'
     ],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
