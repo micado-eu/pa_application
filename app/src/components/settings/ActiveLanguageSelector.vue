6d79a5fe6cc1646390e268bac0318936f0366485
Luca Gioppo <gioppoluca@libero.it>
2020-10-28 17:18:46 +0100

    added weblate button

--- app/src/components/settings/ActiveLanguageSelector.vue
+++ app/src/components/settings/ActiveLanguageSelector.vue
@@ -1,24 +1,44 @@
 <template>
-  <div class="q-pa-md">
+  <div class="col q-gutter-md">
     <h5>{{$t("data_settings.language")}}</h5>
+    <q-card>
+      <q-card-section>
+        <div class="text-h4">{{$t('data_settings.manage_languages')}}</div>
+      </q-card-section>
+      <q-card-section>
+        <q-btn
+          v-for="language in languages"
+          :key="language.isoCode"
+          :outline="!language.active"
+          :style="language.active"
+          class="q-mx-sm language_btn"
+          :label="language.name"
+          @click="setActive(language)"
+          :id="language.isoCode"
+          color="accent"
+          no-caps
+        ></q-btn>
+      </q-card-section>
+    </q-card>
+    <q-card>
+      <q-card-section>
+        <div class="text-h4">{{$t('data_settings.manage_translations')}}</div>
+      </q-card-section>
+      <q-card-section>
 
-    <q-btn
-      v-for="language in languages"
-      :key="language.isoCode"
-      :outline="!language.active"
-      :style="language.active"
-      class="q-mx-sm language_btn"
-      :label="language.name"
-      @click="setActive(language)"
-      :id="language.isoCode"
-      color="accent"
-      no-caps
-    ></q-btn>
+        <q-btn
+          :label="$t('data_settings.send_to_translation')"
+          @click="sendToTranslation()"
+        />
+      </q-card-section>
+    </q-card>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
+import { mapGetters, mapActions } from "vuex"
+import weblateClient from 'api-weblate-client'
+
 export default {
   name: "LanguageSettings",
   computed: {
@@ -26,20 +46,26 @@ export default {
   },
   methods: {
     ...mapActions("language", ["fetchLanguages", "setLanguageActive"]),
-    setActive(language) {
+    setActive (language) {
       let newLanguage = {
         lang: language.lang,
         isoCode: language.isoCode,
         name: language.name,
         active: !language.active
-      };
+      }
       this.setLanguageActive(newLanguage)
+    },
+    sendToTranslation () {
+      weblateClient.sendToTranslation()
+        .then((result) => {
+          console.log(result)
+        })
     }
   },
-  created() {
-    this.fetchLanguages();
+  created () {
+    this.fetchLanguages()
   }
-};
+}
 </script>
 <style lang="scss" scoped>
 .language_btn {
