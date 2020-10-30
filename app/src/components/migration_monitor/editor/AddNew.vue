673ec875bf3d54f650c448f982938ea1192d13de
JowieXiang <zyx1993@126.com>
2020-10-30 14:27:29 +0100

    Migration monitor: editor page style

--- app/src/components/migration_monitor/editor/AddNew.vue
+++ app/src/components/migration_monitor/editor/AddNew.vue
@@ -1,42 +1,42 @@
 <template>
   <div :id="$options.name" class="q-pa-md">
-    <div style="text-align:center;padding-top:30px">
+    <div style="text-align:center;padding-top:30px" class="main">
       <div class="q-pa-lg edit-element-component">
-        <div class="q-pa-xsm row" style="text-align:center; padding-right:45px">
-          <div class="col-4" style="padding-left:40px;">
-            <h5 style="text-align:left;font-size:18px">Chart title</h5>
+        <div class="q-pa-xsm q-mt-md row" style="text-align:center; padding-right:45px">
+          <div class="col-12 q-pl-xl">
+            <p class='label'>Chart Title</p>
           </div>
-          <div class="col-8" style="margin: auto;display: block;">
+          <div class="col-12 q-pl-xl">
             <q-input
               bg-color="grey-3"
               dense
               standout
-              outlined
               v-model="title"
               :rules="[val => !!val || 'Field is required']"
+              placeholder="Enter chart title here"
             />
           </div>
         </div>
-        <div class="q-pa-xsm row" style="text-align:center; padding-right:45px">
-          <div class="col-4" style="padding-left:40px;">
-            <h5 style="text-align:left;font-size:18px">Category</h5>
+        <div class="q-pa-xsm q-mt-md row" style="text-align:center; padding-right:45px">
+          <div class="col-12 q-pl-xl">
+            <p class='label'>Category</p>
           </div>
-          <div class="col-8" style="margin: auto;display: block;">
+          <div class="col-12 q-pl-xl">
             <q-input
               bg-color="grey-3"
               dense
               standout
-              outlined
               v-model="category"
               :rules="[val => !!val || 'Field is required']"
+              placeholder="Select category"
             />
           </div>
         </div>
-        <div class="q-pa-xsm row" style="text-align:center; padding-right:45px">
-          <div class="col-4" style="padding-left:40px;">
-            <h5 style="text-align:left;font-size:18px">Chart type</h5>
+        <div class="q-pa-xsm q-mt-md row" style="text-align:center; padding-right:45px">
+          <div class="col-12 q-pl-xl">
+            <p class='label'>Chart Type</p>
           </div>
-          <div class="col-8" style="margin: auto;display: block;">
+          <div class="col-12 q-pl-xl">
             <q-select
               v-model="type"
               standout
@@ -46,75 +46,62 @@
             />
           </div>
         </div>
-        <div class="q-pa-xsm row" style="text-align:center; padding-right:45px">
-          <div class="col-4" style="padding-left:40px;">
-            <h5 style="text-align:left;font-size:18px">Board</h5>
+        <div class="q-pa-xsm q-mt-md row" style="text-align:center; padding-right:45px">
+          <div class="col-12 q-pl-xl">
+            <p class='label'>Board</p>
           </div>
-          <div class="col-8" style="margin: auto;display: block;">
+          <div class="col-12 q-pl-xl">
             <q-input
               bg-color="grey-3"
               dense
               standout
-              outlined
               v-model="board"
               :rules="[val => !!val || 'Field is required']"
+              placeholder="Enter board name"
             />
           </div>
         </div>
-        <div class="q-pa-xsm row" style="text-align:center; padding-right:45px">
-          <div class="col-4" style="padding-left:40px;">
-            <h5 style="text-align:left;font-size:18px">x axis</h5>
+        <div class="q-pa-xsm q-mt-md row" style="text-align:center; padding-right:45px">
+          <div class="col-6 q-pl-xl q-mr-l">
+            <p class='label'>x Axis</p>
           </div>
-          <div class="col-8" style="margin: auto;display: block;">
+          <div class="col-6 q-pl-xl">
+            <p class='label'>y Axis</p>
+          </div>
+          <div class="col-6 q-pl-xl" >
             <q-input
               bg-color="grey-3"
               dense
               standout
-              outlined
               v-model="x"
               :rules="[val => !!val || 'Field is required']"
             />
           </div>
-        </div>
-        <div class="q-pa-xsm row" style="text-align:center; padding-right:45px">
-          <div class="col-4" style="padding-left:40px;">
-            <h5 style="text-align:left;font-size:18px">y axis</h5>
-          </div>
-          <div class="col-8" style="margin: auto;display: block;">
+          <div class="col-6 q-pl-xl" style="margin: auto;display: block;">
             <q-input
               bg-color="grey-3"
               dense
               standout
-              outlined
               v-model="y"
               :rules="[val => !!val || 'Field is required']"
             />
           </div>
         </div>
-
-        <div class="row" style="padding-bottom:15px;padding-right:45px">
-          <div class="col-4" style="padding-left:40px;">
-            <h5 style="text-align:left;font-size:18px">Chart description</h5>
+        
+        <div class="q-mt-md row" style="padding-bottom:15px;padding-right:45px">
+          <div class="col-12 q-pl-xl">
+            <p class='label'>Chart Description</p>
           </div>
-          <div class="col-8" style="display: block;">
-            <q-input type="textarea" bg-color="grey-3" standout outlined v-model="description" />
-          </div>
-        </div>
-
-        <div class="q-pa-xsm row" style="text-align:center; padding-right:45px">
-          <div class="col-4" style="padding-left:40px;">
-            <h5 style="text-align:left;font-size:18px">X axis is unix-timestamp</h5>
-          </div>
-          <div class="col-8" style="margin: auto;display: block;">
-            <q-checkbox color="accent" v-model="xistime" clickable />
+          <div class="col-12 q-pl-xl">
+            <q-input type="textarea" bg-color="grey-3" standout v-model="description" placeholder="Describe your Chart"/>
           </div>
         </div>
 
-        <div class="q-pa-xsm row" style="text-align:center; padding-right:45px">
-          <div class="col-4" style="padding-left:40px;">
-            <h5 style="text-align:left;font-size:18px">Data format</h5>
+        <div class="q-pa-xsm q-mt-md row" style="text-align:center; padding-right:45px">
+          <div class="col-12 q-pl-xl">
+            <p class='label'>Data Format</p>
           </div>
-          <div class="col-8" style="margin: auto;display: block;">
+          <div class="col-12 q-pl-xl">
             <q-select
               v-model="data_format"
               standout
@@ -127,20 +114,19 @@
         </div>
         <div
           v-if="data_format === 'csv'"
-          class="q-pa-xsm row"
+          class="q-pa-xsm q-mt-md row"
           style="text-align:center;padding-right:45px"
         >
-          <div class="col-4" style="padding-left:40px;">
-            <h5 style="text-align:left;font-size:18px">Choose csv</h5>
+          <div class="col-12 q-pl-xl">
+            <p class='label'>Choose csv</p>
           </div>
-          <div class="col" style="margin: auto;display: block;">
+          <div class="col-12 q-pl-xl">
             <q-file
               v-model="filename"
               @input="getFiles"
               bg-color="grey-3"
               dense
               standout
-              outlined
               :rules="[val => !!val || 'Field is required']"
             ></q-file>
           </div>
@@ -148,20 +134,19 @@
 
         <div
           v-if="data_format === 'JSON'"
-          class="q-pa-xsm row"
+          class="q-pa-xsm q-mt-md row"
           style="text-align:center;padding-right:45px"
         >
-          <div class="col-4" style="padding-left:40px;">
-            <h5 style="text-align:left;font-size:18px">Choose JSON</h5>
+          <div class="col-12 q-pl-xl">
+            <p class='label'>Choose JSON</p>
           </div>
-          <div class="col" style="margin: auto;display: block;">
+          <div class="col-12 q-pl-xl">
             <q-file
               v-model="filename"
               @input="getFiles"
               bg-color="grey-3"
               dense
               standout
-              outlined
               :rules="[val => !!val || 'Field is required']"
             ></q-file>
           </div>
@@ -169,18 +154,17 @@
 
         <div
           v-if="data_format === 'API'"
-          class="q-pa-xsm row"
+          class="q-pa-xsm q-mt-md row"
           style="text-align:center; padding-right:45px"
         >
-          <div class="col-4" style="padding-left:40px;">
-            <h5 style="text-align:left;font-size:18px">API address</h5>
+          <div class="col-12 q-pl-xl">
+            <p class='label'>API address</p>
           </div>
-          <div class="col-8" style="margin: auto;display: block;">
+          <div class="col-12 q-pl-xl">
             <q-input
               bg-color="grey-3"
               dense
               standout
-              outlined
               v-model="url"
               :rules="[val => !!val || 'Field is required']"
             />
@@ -191,20 +175,22 @@
       <div class="row">
         <div class="q-pa-md col-6" style="text-align:right">
           <q-btn
-            color="accent"
+            class="delete-button"
             unelevated
+            no-caps
+            size='md'
             label="Cancel"
-            style="width:100px"
             @click="reset()"
             to="/situation/editor"
           />
         </div>
         <div class="q-pa-md col-6" style="text-align:left">
           <q-btn
-            color="info"
+            color="accent"
             unelevated
-            label="Save/Update"
-            style="width:150px"
+            no-caps
+            size='md'
+            label="Save"
             @click="addChart()"
           />
         </div>
@@ -226,19 +212,19 @@ export default {
       url: "",
       xistime: false,
       data_format: "JSON",
-      formats: ["JSON", "csv", "API"],
+      formats: ["JSON", "csv"],
       category: "",
       type: "",
       types: ["BAR", "LINE", "PIE"],
       board: "",
       content: null,
-      filename: null,
-    };
+      filename: null
+    }
   },
   computed: {
     categories() {
-      return this.$store.state.statistics.categories.map((c) => c.category);
-    },
+      return this.$store.state.statistics.categories.map((c) => c.category)
+    }
   },
   methods: {
     addChart: function () {
@@ -252,46 +238,46 @@ export default {
         xistime: this.xistime,
         x: this.x,
         y: this.y,
-        board: this.board,
-      };
+        board: this.board
+      }
       this.$store
         .dispatch("statistics/addChart", data)
         .then(() => {
           this.$q.dialog({
-            message: "upload succeeds, please refresh to see new charts",
-          });
+            message: "upload succeeds, please refresh to see new charts"
+          })
           // this.$router.push("/situation/editor");
         })
         .catch((err) => {
           this.$q.dialog({
-            message: err,
-          });
-        });
+            message: err
+          })
+        })
     },
     reset: function () {
-      this.title = "";
-      this.description = "";
-      this.x = "";
-      this.y = "";
-      this.url = "";
-      this.xistime = false;
-      this.data_format = "JSON";
-      this.category = "";
-      this.type = "";
-      this.board = "";
-      this.content = null;
+      this.title = ""
+      this.description = ""
+      this.x = ""
+      this.y = ""
+      this.url = ""
+      this.xistime = false
+      this.data_format = "JSON"
+      this.category = ""
+      this.type = ""
+      this.board = ""
+      this.content = null
     },
     onChangeFileType: function (e) {
-      this.filename = null;
-      this.content = null;
+      this.filename = null
+      this.content = null
     },
     getFiles(file) {
       if (file != undefined) {
-        this.filename = file;
-        let reader = new FileReader();
+        this.filename = file
+        let reader = new FileReader()
         // Convert the file to base64 text
         // reader.readAsDataURL(file);
-        reader.readAsText(file);
+        reader.readAsText(file)
         reader.onload = () => {
           // Make a fileInfo Object
           let fileInfo = {
@@ -299,21 +285,36 @@ export default {
             type: file.type,
             size: Math.round(file.size / 1000) + " kB",
             result: reader.result,
-            file: file,
-          };
-          this.content = fileInfo.result;
-        };
+            file: file
+          }
+          this.content = fileInfo.result
+        }
       } else {
-        this.content = null;
+        this.content = null
       }
-    },
-  },
-};
+    }
+  }
+}
 </script>
 <style lang="scss" scoped>
 $btn_secondary: #cdd0d2;
 .edit-element-component {
   border: 1px solid $btn_secondary;
   border-radius: 10px;
+}
+.main{
+  padding: 16px 210px;
+}
+.label{
+  text-align:left;
+  font-size:18px
+}
+.delete-button {
+  background-color: white;
+  color: black;
+  border: 1px solid #c71f40;
+}
+.q-btn{
+  width: 135px;
 }
 </style>
