7711970ddcb9dc85a2f7288b014b11bef3c507ba
Timo Truyts <truyts.timo@gmail.com>
2020-11-11 11:41:17 +0100

    Allow selecting range

--- app/src/components/StatsBoard.vue
+++ app/src/components/StatsBoard.vue
@@ -1,26 +1,51 @@
 <template>
+
+
+
+
+
   <div>
-    <q-card class="stat-element">
+    <q-btn-toggle
+      v-model="statsIndex"
+      toggle-color="secondary"
+      :options="[
+        {label: $t('stats.today'), value: 'today'},
+        {label: $t('stats.7days'), value: '7days'},
+        {label: $t('stats.30days'), value: '30days'}
+      ]"
+    />
+
+    <q-card class="stat-element" v-if="selectedStats">
       <q-card-section>
-        <div>{{stats['7days'].period}}</div>
+        <div>{{selectedStats.period}}</div>
       </q-card-section>
+
       <q-card-section class="row q-gutter-md">
         <StatElement
-          :data="stats['7days'].dashboard.avg_requests"
-          :title="$t('stats.avg_requests')"
+          :data="selectedStats.dashboard.total_sessions"
+          :title="$t('stats.total_sessions')"
         />
         <StatElement
-          :data="stats['7days'].dashboard.avg_time"
-          :title="$t('stats.avg_time')"
+          :data="selectedStats.dashboard.total_users"
+          :title="$t('stats.total_users')"
         />
         <StatElement
-          :data="stats['7days'].dashboard.new_users"
+          :data="selectedStats.dashboard.new_users"
           :title="$t('stats.new_users')"
         />
         <StatElement
-          :data="stats['7days'].dashboard.total_users"
-          :title="$t('stats.total_users')"
+          :data="selectedStats.dashboard.total_time"
+          :title="$t('stats.time_spent')"
         />
+        <StatElement
+          :data="selectedStats.dashboard.avg_time"
+          :title="$t('stats.avg_time')"
+        />
+        <StatElement
+          :data="selectedStats.dashboard.avg_requests"
+          :title="$t('stats.avg_requests')"
+        />
+
       </q-card-section>
     </q-card>
   </div>
@@ -37,7 +62,14 @@ export default {
     StatElement
   },
   data () {
-    return {}
+    return {
+      statsIndex: '7days' // The index to use in the stats, so we can switch between today,7days,30days.
+    }
+  },
+  computed: {
+    selectedStats () {
+      return this.stats[this.statsIndex]
+    }
   }
 }
 </script>
