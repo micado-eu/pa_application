40e6700092631baf9d7228424675d685d51aa31e
Iván Chicano <ivchicano@gmail.com>
2021-02-04 15:07:22 +0100

    Add visualization of creator of item in information centre, events and glossary

--- app/src/components/ListSearchTags.vue
+++ app/src/components/ListSearchTags.vue
@@ -316,6 +316,14 @@
                         {{$t("lists.location")}}: {{item.location}}
                       </span>
                     </div>
+                    <div>
+                      <span
+                        class="date-text q-mt-sm q-mr-xl"
+                        v-if="item.creator && showExtraInfo[item.id]"
+                      >
+                        {{$t("lists.creator")}}: {{item.creator}}
+                      </span>
+                    </div>
                   </template>
                 </glossary-editor-viewer>
               </q-item-section>
