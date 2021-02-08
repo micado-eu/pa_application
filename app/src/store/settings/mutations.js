3ca52ad6be86dfc06b482fadb49fa9e6e6f41d48
giuseppecsi <giuseppe.dorio@csi.it>
2021-02-08 11:59:07 +0100

    Added field in settings to add email for feedback to PA

--- app/src/store/settings/mutations.js
+++ app/src/store/settings/mutations.js
@@ -13,6 +13,11 @@ export function setSetting (state, setting) {
     state.settings.find(set => set.key === setting.key).value = setting.value
   }
 }
+export function setNewSetting (state, setting) {
+  
+    state.settings.push(setting)
+  
+}
 export function setLastRefreshn (state, last_refresh) {
   if (last_refresh) {
     state.last_refresh = last_refresh
