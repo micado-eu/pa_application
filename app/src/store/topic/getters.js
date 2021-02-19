db86c9f6d9e6340664b3e5dbad4156fb05fc0017
giuseppecsi <giuseppe.dorio@csi.it>
2021-02-19 10:23:10 +0100

    Added getter for topic tree select

--- app/src/store/topic/getters.js
+++ app/src/store/topic/getters.js
@@ -1,3 +1,8 @@
+import editEntityMixin from '../../mixin/editEntityMixin'
+import { i18nInstance } from 'boot/i18n.js' 
+import { arrayToTree } from "performant-array-to-tree";
+
+
 export function topic(state) {
     return state.topic
   }
@@ -6,3 +11,16 @@ export function topic_published(state) {
       return topico.published == true
     })
   }
+  export function tree_options(state){
+    var options = []
+  var published_topi = state.topic.filter((topico)=>{
+    return topico.published == true
+  })
+  published_topi.forEach((top)=>{
+    options.push({id:top.id, parentId:top.father, label:top.translations.filter(editEntityMixin.methods.filterTranslationModel((i18nInstance.locale)))[0].topic})
+  })
+  const tree = arrayToTree(options,{ dataField: null })
+  console.log(tree)
+  return tree
+  }
+
