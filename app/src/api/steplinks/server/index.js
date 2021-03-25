ffff25703592611261dbe30c069a02de280cffaa
giuseppecsi <giuseppe.dorio@csi.it>
2021-03-25 09:42:32 +0100

    steplinks are now published together with process and steps

--- app/src/api/steplinks/server/index.js
+++ app/src/api/steplinks/server/index.js
@@ -16,5 +16,29 @@ export default {
       .get('/backend/1.0.0/step-links?filter[include][0][relation]=translations&filter[where][idProcess]=' + id)
       .then(response => response.data)
       .catch(error_handler);
-  }
+  },
+  updateSteplinkTranslation (translation, id) {
+    translation.id = id
+    const savingTranslation = JSON.parse(JSON.stringify(translation, ['id','translationState']));
+
+    // create fake id here
+    return axiosInstance
+      .patch('/backend/1.0.0/step-links/' +id +'/step-link-translations', savingTranslation)
+      .then(response => response.data)
+      .catch(error_handler);
+  },
+  saveSteplinkTranslationProd (id) {
+    // create fake id here
+    return axiosInstance
+      .get('/backend/1.0.0/step-links/to-production?id=' + id)
+      .then(response => response.data)
+      .catch(error_handler);
+  },
+  deleteSteplinkTranslationProd (id) {
+    // create fake id here
+    return axiosInstance
+      .delete('/backend/1.0.0/step-links/' + id + '/step-link-translation-prods')
+      .then(response => response.data)
+      .catch(error_handler);
+  },
 }
