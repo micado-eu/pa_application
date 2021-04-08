6e41485f5e6896f9decc9dfac9f0c61869826c8b
Luca Gioppo <gioppoluca@libero.it>
2021-04-08 08:31:45 +0200

    added surveycreator

---
+++ app/src/components/SurveyCreator.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="surveyCreatorContainer"></div>
+</template>
+
+<script>
+import * as SurveyCreator from "survey-creator";
+import "survey-creator/survey-creator.css";
+import * as SurveyKo from "survey-knockout";
+import * as widgets from "surveyjs-widgets";
+//import { init as customWidget } from "../components/customwidget";
+// widgets.icheck(SurveyKo);
+/*
+widgets.select2(SurveyKo);
+widgets.inputmask(SurveyKo);
+widgets.jquerybarrating(SurveyKo);
+widgets.jqueryuidatepicker(SurveyKo);
+widgets.nouislider(SurveyKo);
+widgets.select2tagbox(SurveyKo);
+widgets.sortablejs(SurveyKo);
+widgets.ckeditor(SurveyKo);
+widgets.autocomplete(SurveyKo);
+widgets.bootstrapslider(SurveyKo);
+//customWidget(SurveyKo);
+SurveyKo.Serializer.addProperty("question", "tag:number");
+var CkEditor_ModalEditor = {
+  afterRender: function (modalEditor, htmlElement) {
+    var editor = window["CKEDITOR"].replace(htmlElement);
+    editor.on("change", function () {
+      modalEditor.editingValue = editor.getData();
+    });
+    editor.setData(modalEditor.editingValue);
+  },
+  destroy: function (modalEditor, htmlElement) {
+    var instance = window["CKEDITOR"].instances[htmlElement.id];
+    if (instance) {
+      instance.removeAllListeners();
+      window["CKEDITOR"].remove(instance);
+    }
+  }
+};
+SurveyCreator.SurveyPropertyModalEditor.registerCustomWidget(
+  "html",
+  CkEditor_ModalEditor
+);
+*/
+export default {
+  name: "survey-creator",
+  data () {
+    return {};
+  },
+  mounted () {
+    let options = { showEmbededSurveyTab: true };
+    this.surveyCreator = new SurveyCreator.SurveyCreator(
+      "surveyCreatorContainer",
+      options
+    );
+    this.surveyCreator.saveSurveyFunc = function () {
+      console.log(JSON.stringify(this.text));
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
