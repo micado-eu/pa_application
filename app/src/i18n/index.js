1ccd356f18e90ae69bff733fa18db529c412cb50
Iván Chicano <ivchicano@gmail.com>
2021-04-27 10:49:07 +0200

    Fix translations file not having correct imports

--- app/src/i18n/index.js
+++ app/src/i18n/index.js
@@ -2,6 +2,9 @@ import enUS from './en-us'
 import it from './it-it'
 import ar from './ar'
 import de from './de'
+import es from './es'
+import nl from './nl'
+import zh from './zh'
 
 export default {
   en: enUS,
