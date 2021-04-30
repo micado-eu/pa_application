c9d50b8d22f2da637e27780f5c3e2432840ff5d3
Luca Gioppo <gioppoluca@libero.it>
2021-04-30 11:35:06 +0200

    missing es and nl in i18

--- app/src/i18n/index.js
+++ app/src/i18n/index.js
@@ -1,12 +1,15 @@
 import enUS from './en-us'
 import it from './it-it'
 import ar from './ar'
+import nl from './nl'
+import es from './es'
 import de from './de'
 
 export default {
   en: enUS,
   it,
   ar,
+  nl,
+  es,
   de
-
 }
