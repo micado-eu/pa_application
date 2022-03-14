04c8c9795fccd92e0fa450acccdff58bb80a1f45
D'ORIO Giuseppe 2098 <giuseppe.dorio@csi.it>
2022-03-14 12:33:22 +0100

    fixed broken login

--- app/src/api/user/server/index.js
+++ app/src/api/user/server/index.js
@@ -27,7 +27,7 @@ export default {
   fetchUserLogin (username, tenant) {
     console.log(username, tenant)
     return axiosInstance
-      .get('/backend/1.0.0/users?filter[include][0][relation]=attributes&filter[include][1][relation]=interventionPlans&filter[where][and][0][umUserName][eq]=' + username + '&filter[where][and][1][umTenantId]=' + tenant + '&filter[fields]=umId')
+      .get('/backend/1.0.0/users?filter[include][0][relation]=attributes&filter[include][1][relation]=interventionPlans&filter[where][and][0][umUserName][eq]=' + username + '&filter[where][and][1][umTenantId]=' + tenant )
       .then(response => { return response.data })
       .catch(error_handler)
   },
