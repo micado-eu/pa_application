c848dfe71dbf9ede7ff63a194b4ad8be06e01811
balcse <cserpesbalazs@gmail.com>
2022-03-24 16:03:12 +0100

    adapted layout of MSM overview

--- app/src/components/migration_monitor/Main.vue
+++ app/src/components/migration_monitor/Main.vue
@@ -1,52 +1,52 @@
 <template>
   <div>
-    <div
-      style="
-        font-style: normal;
-        height: 72px;
-        text-align: center;
-        padding-top: 15px;
-        font-weight: bold;
-        font-size: 30px;
-        line-height: 41px;
-        color: white;
-        background-color: #ff7c44;
-      "
-    >
-      {{$t("migration_monitor.title")}}
+    <div v-if="loading">
+      {{ $t('input_labels.loading') }}
     </div>
+    <div v-else>
+      <div id="content">
+        <div
+          class=" q-pa-md image "
+          style="text-align:center"
+        >
+          <div
+            class=" top-div"
+            style=""
+          >
+            {{ $t('menu.situation') }}
+            <q-icon
+              name="img:statics/msm_header.svg"
+              class="top-icon"
+            />
+          </div>
+        </div>
+        <hr style="border: 1px solid #0F3A5D">
     <div class="q-pa-md row  justify-center">
       <q-btn
         no-caps
         class="q-ml-md"
-        color="secondary"
+        color="accent"
         size="md"
         :label="$t('button.situation_manage_charts')"
         to="/situation/overview"
       />
-      <q-btn
-        no-caps
-        class="q-ml-md q-mr-md"
-        color="white"
-        text-color="black"
-        size="md"
-        :label="$t('button.situation_about')"
-        to="#"
-      />
     </div>
     <div class="q-pa-md row">
       <div v-for="(b, i) in boards" :key="i" class="col-4">
-        <q-card class="q-ma-md">
-          <q-card-section class="icon-div">
-            <q-icon :name="b.icon" style="font-size: 5em" />
-          </q-card-section>
+        <q-card 
+        bordered
+        class="q-ma-md boardcard">
           <q-card-section class="title-div">
             <div class="text-h6" @click="jump(b.link)">{{ b.title }}</div>
           </q-card-section>
-          <q-separator />
+          <q-separator
+            color="accent"
+            size="1pt"
+           />
           <q-card-actions class="card-body" vertical align="left" >
             <q-btn
               v-for="(cat, i) in getCategory(b)"
+              class="categorytitle"
               :key="i"
               flat
               @click="onClickNav(b.title, cat.category)"
@@ -56,6 +56,8 @@
         </q-card>
       </div>
     </div>
+  </div>
+  </div>
   </div>
 </template>
 
@@ -95,16 +97,32 @@ export default {
 }
 </script>
 <style scoped>
-.card {
+
+.boardcard {
   margin: auto;
   width: 97%;
   margin-top: 20px;
-  border-radius: 5px;
-  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+  border: 1pt solid var(--q-color-accent);
+  /* box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2); */
 }
 .icon-div {
   text-align: center;
 }
+
+.text-h6 {
+  color: var(--q-color-accent);
+  font-weight: bolder;
+  padding-top: 1em;
+  padding-bottom: 1em;
+}
+
+.categorytitle {
+  text-transform: none;
+  margin-left: auto;
+  margin-right: auto;
+}
+
 .title-div {
   text-align: center;
   cursor: pointer;
@@ -114,5 +132,22 @@ export default {
 }
 .card-body{
   background-color: #F1F8FC;
+}
+
+.top-icon{
+width:360px;
+  height:100%
+}
+.top-div{
+  font-style: normal;
+  padding-top:15px;
+  padding-left:30px;
+  font-weight: bold;
+  font-size: 40px;
+  line-height: 54px;
+  color:#0F3A5D; 
+}
+.image{
+  background-image: url("/statics/BG Pattern.svg");
 }
 </style>
